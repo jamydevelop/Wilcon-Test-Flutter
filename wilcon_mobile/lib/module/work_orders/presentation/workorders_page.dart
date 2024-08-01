4dd36868a7b10daeb7ec242819b11eafe423a888
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-02 05:36:28 +0800

    add shadow effects on Appbar container

--- wilcon_mobile/lib/module/work_orders/presentation/workorders_page.dart
+++ wilcon_mobile/lib/module/work_orders/presentation/workorders_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:wilcon_mobile/module/work_orders/presentation/widget/buttons_container.dart';
-import 'package:wilcon_mobile/widgets/card_temporary_data.dart';
 import 'package:wilcon_mobile/widgets/appbar_widget.dart';
 import 'package:wilcon_mobile/widgets/back_screen_widget.dart';
-import 'package:wilcon_mobile/widgets/sidebar_widget.dart';
+import 'package:wilcon_mobile/widgets/buttons_widget.dart';
+import 'package:wilcon_mobile/widgets/card_temporary_data.dart';
 import 'package:wilcon_mobile/widgets/search_bar_widget.dart';
-import '../../../widgets/header_bar_widget.dart';
+import 'package:wilcon_mobile/widgets/sidebar_widget.dart';
+import 'package:wilcon_mobile/widgets/header_bar_widget.dart';
 
 class WorkOrdersPage extends StatefulWidget {
   const WorkOrdersPage({super.key});
@@ -15,30 +16,61 @@ class WorkOrdersPage extends StatefulWidget {
 }
 
 class _WorkOrdersPageState extends State<WorkOrdersPage> {
+  final ButtonClass buttonClass = ButtonClass();
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        endDrawer: const CustomSidebar(),
-        body: Padding(
-          padding:
-              const EdgeInsets.symmetric(horizontal: 12.0), // Adjust padding
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              const SizedBox(height: 24),
-              const CustomizedAppBar(),
-              const BackToPrevScreen(),
-              const HeaderBar(text: 'Work Orders'),
-              const SizedBox(height: 12),
-              ButtonContainer(),
-              const SizedBox(height: 12),
-              const SearchBarWidget(hintText: 'Search'),
-              const SizedBox(height: 12),
-              const Expanded(
-                child: Center(child: CardTemporaryData()),
-              ),
-            ],
+    return Scaffold(
+      body: SafeArea(
+        child: Scaffold(
+          endDrawer: const CustomSidebar(),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 0.0), // Remove left and right padding
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Background color
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1), // Shadow color
+                        spreadRadius: 2, // Spread radius
+                        blurRadius: 5, // Blur radius
+                        offset: const Offset(0, 3), // Shadow position
+                      ),
+                    ],
+                    borderRadius: BorderRadius.zero, // Remove rounded corners
+                  ),
+                  padding: const EdgeInsets.all(
+                      12.0), // Padding inside the container
+                  child: Column(
+                    children: [
+                      const CustomizedAppBar(),
+                      const BackToPrevScreen(),
+                      const HeaderBar(text: 'Work Orders'),
+                      const SizedBox(height: 12),
+                      const SearchBarWidget(hintText: 'Search'),
+                      const SizedBox(height: 12),
+                      ButtonContainer(),
+                      // buttonClass.filterButton(context),
+                      const SizedBox(height: 24),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal:
+                              12.0), // Add 12px margin on left and right
+                      child: CardTemporaryData(), // body: ,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
