ea8e994ca5ac6f566914fe398b811fb420cc3678
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-06 01:11:38 +0800

    Update checklist_page.dart

--- wilcon_mobile/lib/module/checklist/presentation/checklist_page.dart
+++ wilcon_mobile/lib/module/checklist/presentation/checklist_page.dart
@@ -3,6 +3,7 @@ import 'package:wilcon_mobile/module/checklist/presentation/widget/checklist_ite
 import 'package:wilcon_mobile/module/checklist/presentation/widget/four_buttons_example_widget.dart';
 import 'package:wilcon_mobile/widgets/appbar_widget.dart';
 import 'package:wilcon_mobile/widgets/back_screen_widget.dart';
+import 'package:wilcon_mobile/widgets/search_bar_widget.dart';
 import 'package:wilcon_mobile/widgets/sidebar_widget.dart';
 import 'package:wilcon_mobile/widgets/header_bar_widget.dart';
 
