90be45e3286b87629a22c5ccb295cd3e07330ef6
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-16 03:55:16 +0800

    change title of the app

--- wilcon_mobile/lib/main.dart
+++ wilcon_mobile/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Wilcon Depot Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade900),
         useMaterial3: true,
