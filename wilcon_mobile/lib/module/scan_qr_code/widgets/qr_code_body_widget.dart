6775420877ace4e7a0d9fbb30651e5c01af45215
John Ace Magno <127085144+jamydevelop@users.noreply.github.com>
2024-08-18 19:31:28 +0800

    QR Code scanner overlay

--- wilcon_mobile/lib/module/scan_qr_code/widgets/qr_code_body_widget.dart
+++ wilcon_mobile/lib/module/scan_qr_code/widgets/qr_code_body_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:wilcon_mobile/module/scan_qr_code/widgets/qrscanneroverlay.dart';
 
 class QrCodeBodyWidget extends StatelessWidget {
   const QrCodeBodyWidget({super.key});
@@ -15,39 +16,43 @@ class QrCodeBodyWidget extends StatelessWidget {
               detectionSpeed: DetectionSpeed.noDuplicates,
             ),
             onDetect: (capture) {
+              // Handle barcode detection here
               // final List<Barcode> barcodes = capture.barcodes;
-
               // for (final barcode in barcodes) {
               //   print('Barcode Found! ${barcode.rawValue}');
               // }
             },
           ),
-          // Centered frame with a thickness of 4px
-          Center(
-            child: Container(
-              width: 400,
-              height: 400,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.green,
-                  width: 4, // Set the thickness to 4px
-                ),
-                // Remove borderRadius to make the edges sharp
-                borderRadius: BorderRadius.zero,
-              ),
-              // Optional: Add a child widget if needed, for example, text
-              // child: Center(
-              //   child: Text(
-              //     'Scan Here',
-              //     style: TextStyle(
-              //       color: Colors.green,
-              //       fontSize: 16,
-              //       fontWeight: FontWeight.bold,
-              //     ),
-              //   ),
-              // ),
-            ),
+          // QRScannerOverlay on top of the camera view
+          QRScannerOverlay(
+            overlayColour: Colors.black.withOpacity(0.5),
           ),
+          // Centered frame with a thickness of 4px
+          // Center(
+          //   child: Container(
+          //     width: 400,
+          //     height: 400,
+          //     decoration: BoxDecoration(
+          //       border: Border.all(
+          //         color: Colors.green,
+          //         width: 4, // Set the thickness to 4px
+          //       ),
+          //       // Remove borderRadius to make the edges sharp
+          //       borderRadius: BorderRadius.zero,
+          //     ),
+          //     // Optional: Add a child widget if needed, for example, text
+          //     // child: Center(
+          //     //   child: Text(
+          //     //     'Scan Here',
+          //     //     style: TextStyle(
+          //     //       color: Colors.green,
+          //     //       fontSize: 16,
+          //     //       fontWeight: FontWeight.bold,
+          //     //     ),
+          //     //   ),
+          //     // ),
+          //   ),
+          // ),
         ],
       ),
     );
